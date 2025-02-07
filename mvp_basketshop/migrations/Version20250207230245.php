2255347fbfafb5173a658fe102c4b68b0b4acc7c
fridev <magrigsdesign@gmail.com>
2025-02-08 00:07:22 +0100

    create tables marques and roles

---
+++ mvp_basketshop/migrations/Version20250207230245.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250207230245 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'creation des tables marques et roles';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE marques (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nom VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE TABLE roles (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nom VARCHAR(255) NOT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE marques');
+        $this->addSql('DROP TABLE roles');
+    }
+}
