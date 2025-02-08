4cf39655e236e7f95c66a5a6a530e4a78215c97d
fridev <magrigsdesign@gmail.com>
2025-02-08 01:23:05 +0100

    create table utilisateur

--- mvp_basketshop/migrations/Version20250207230245.php
+++ mvp_basketshop/migrations/Version20250207230245.php
@@ -7,9 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
+
 final class Version20250207230245 extends AbstractMigration
 {
     public function getDescription(): string
--- mvp_basketshop/src/Entity/Roles.php
+++ mvp_basketshop/src/Entity/Roles.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\RolesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: RolesRepository::class)]
@@ -15,6 +17,17 @@ class Roles
 
     #[ORM\Column(length: 255)]
     private ?string $nom = null;
+
+    /**
+     * @var Collection<int, Utilisateur>
+     */
+    #[ORM\OneToMany(targetEntity: Utilisateur::class, mappedBy: 'roleId', orphanRemoval: true)]
+    private Collection $utilisateurs;
+
+    public function __construct()
+    {
+        $this->utilisateurs = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +42,36 @@ class Roles
     public function setNom(string $nom): static
     {
         $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Utilisateur>
+     */
+    public function getUtilisateurs(): Collection
+    {
+        return $this->utilisateurs;
+    }
+
+    public function addUtilisateur(Utilisateur $utilisateur): static
+    {
+        if (!$this->utilisateurs->contains($utilisateur)) {
+            $this->utilisateurs->add($utilisateur);
+            $utilisateur->setRoleId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUtilisateur(Utilisateur $utilisateur): static
+    {
+        if ($this->utilisateurs->removeElement($utilisateur)) {
+            // set the owning side to null (unless already changed)
+            if ($utilisateur->getRoleId() === $this) {
+                $utilisateur->setRoleId(null);
+            }
+        }
 
         return $this;
     }
